1712e7e48a59f932891fc2b716dc66c1abcdc539
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-08 16:16:51 -0500

    Basic types in Ts

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,45 @@
 console.log("hello, TypeScript");
 
-function add(a: number, b: number) {
-  return a + b;
+// function add(a: number, b: number) {
+//   return a + b;
+// }
+
+// const sum = add(2, 3);
+// console.log(sum);
+
+let muted: boolean = true;
+muted = false;
+
+//Números
+let numerador = 42;
+let denominador = 6;
+let resultado = numerador / denominador;
+
+//String
+let nombre: string = "Hernan";
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+
+// Arreglos de strings y numeros
+let peopleAndNumbers: Array<string | number> = [];
+
+// Enum
+enum Color {
+  Rojo,
+  Verde,
+  Azul,
 }
 
-const sum = add(2, 3);
+let colorFavorito: Color = Color.Rojo;
+
+//Any keyboard
+let comodin: any = "Joker";
+
+comodin = { type: "Wildcard" };
+
+// Object
+
+let someObject: object = { type: "Wildcard" };
