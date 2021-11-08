40e25ce3c358c521b73aead9bd16333acedbf9f1
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-08 13:44:07 -0500

    Interception observer

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,12 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay()],
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 button.onclick = () => player.togglePlay();
