a83491532b2f1f4bb4b3f7927ff05a6d111dc269
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-09 09:48:59 -0500

    Implementing singleton pattern desing0

--- ejercicios/singleton/Singleton.ts
+++ ejercicios/singleton/Singleton.ts
@@ -1,15 +1,12 @@
 class Singleton {
   private static instance: Singleton;
-
   private constructor() {
-    // init
+    //init
   }
-
   static getInstance() {
     if (!Singleton.instance) {
       Singleton.instance = new Singleton();
     }
-
     return Singleton.instance;
   }
 }
